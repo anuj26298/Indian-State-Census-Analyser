108ec12cc06572f6cb56c30cd89ee5a6b6d6e811
anuj26298 <kumar26298anuj@gmail.com>
2021-06-15 09:59:26 +0530

    ADDED code to load csv data using iterator

--- src/main/java/com/censusanalyser/CSVStateCensus.java
+++ src/main/java/com/censusanalyser/CSVStateCensus.java
@@ -1,5 +1,27 @@
 package com.censusanalyser;
 
-public class CSVStateCensus {
+import com.opencsv.bean.CsvBindByName;
 
+public class CSVStateCensus {
+    @CsvBindByName(column = "State", required = true)
+    String state;
+
+    @CsvBindByName(column = "Population", required = true)
+    String population;
+
+    @CsvBindByName(column = "AreaInSqKm", required = true)
+    String areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    String densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "CSVStateCensus{" +
+                "state='" + state + '\'' +
+                ", population='" + population + '\'' +
+                ", areaInSqKm='" + areaInSqKm + '\'' +
+                ", densityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/censusanalyser/StateCensusAnalyser.java
@@ -1,4 +1,32 @@
 package com.censusanalyser;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class StateCensusAnalyser {
+
+    public int loadStateCensusData(String csvFilePath){
+        int numberOfEntries = 0;
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder<CSVStateCensus>(reader)
+                    .withType(CSVStateCensus.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVStateCensus> csvStateCensusIterator = csvToBean.iterator();
+            while (csvStateCensusIterator.hasNext()){
+                numberOfEntries++;
+                csvStateCensusIterator.next();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numberOfEntries;
+    }
 }
