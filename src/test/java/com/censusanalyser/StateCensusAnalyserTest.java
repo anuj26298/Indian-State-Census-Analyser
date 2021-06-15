33a79dac2b3945988805dee786ff1cf3087d4483
anuj26298 <kumar26298anuj@gmail.com>
2021-06-15 18:12:35 +0530

    ADDED TestCase 2 : Invalid File Path, checking for handling exception

--- src/test/java/com/censusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/censusanalyser/StateCensusAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.censusanalyser;
 
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,10 +9,24 @@ import java.io.IOException;
 public class StateCensusAnalyserTest {
 
     @Test
-    public void givenCSVFile_WhenCheckedForNumberOfEntries_ReturnTrueIfCorrectNumberOfEntries(){
+    public void givenCSVFile_WhenCheckedForNumberOfEntries_ReturnTrueIfCorrectNumberOfEntries() throws StateCensusAnalyserExceptions {
         String STATE_CENSUS_FILE_PATH = "C:\\Users\\ANUJ\\IdeaProjects\\Indian State Census Analyser" +
                 "\\src\\main\\resources\\StateCensusData.csv";
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
-        Assert.assertEquals(29, stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_FILE_PATH));
+            Assert.assertEquals(29, stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_FILE_PATH));
     }
+        @Test
+    public void givenPathName_WhenWrongFileGiven_ReturnWrongFileException() {
+        String STATE_CENSUS_WRONG_FILE_PATH = "C:\\Users\\ANUJ\\IdeaProjects\\Indian State Census Analyser" +
+                "\\src\\main\\resources\\StateCensusData";
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+
+        try {
+            int numberOfEntries = stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_WRONG_FILE_PATH);
+        } catch (StateCensusAnalyserExceptions e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals(StateCensusAnalyserExceptions.ExceptionType.WRONG_FILE, e.exceptionType);
+        }
+    }
+
 }
